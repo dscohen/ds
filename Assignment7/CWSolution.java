efcdad6c718ccae19607bcd483512693ddb43813
dsc381 <dsc381@nyu.edu>
2013-12-04 12:52:22 -0500

    adding Assignment7

---
+++ Assignment7/CWSolution.java
@@ -0,0 +1,17 @@
+//YOU SHOULD MODIFY THIS FILE.
+import java.util.List;
+
+public class CWSolution {
+
+    public CWSolution(List<String> allWords)
+    {
+	//IMPLEMENT THIS FUNCTION
+    }
+
+    public List<String> solutions(String pattern, 
+				  int maxRequired)
+    {
+        //IMPLEMENT THIS FUNCTION
+    }
+}
+
